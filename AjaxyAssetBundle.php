9d7b4324dcbea6b7cd7347ab36a8f57b2866abd5
Emad Omar <emad2030@gmail.com>
2015-04-26 17:48:34 +0300

    Fixed the class name

--- AjaxyAssetBundle.php
+++ AjaxyAssetBundle.php
@@ -1,20 +1,25 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @link https://github.com/EmadOmar/nozzha-ajaxy/
+ * @author Emad Omar <emad2030@gmail.com>
+ * @license https://github.com/EmadOmar/nozzha-ajaxy/blob/master/LICENSE The MIT License (MIT)
  */
 
 namespace nozzha\ajaxy;
 
 use yii\web\View;
 
-class AssetBundle extends \yii\web\AssetBundle {
+/**
+ * TODO document this class (overview)
+ * @since 0.1
+ */
+class AjaxyAssetBundle extends \yii\web\AssetBundle {
 
     public $sourcePath = '@vendor/nozzha/nozzha-ajaxy/';
     public $css = [];
     public $js = [
+        // FIXME should be added as a dependency instead
         'ext/bootbox_v4.3.0/js/bootbox.min.js',
         'js/nozzha-ajaxy.js'
     ];
